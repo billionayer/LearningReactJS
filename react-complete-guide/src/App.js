5498aa5239bee2edb7f89ed3439683ebf6beba22
Eric Simmons <eric.simmons@aarons.com>
2018-07-13 16:42:28 -0400

    Section 3, Lecture 32

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I am a React App!</h1>
-        <p>This is really working!</p>
+        <Person />
       </div>
     );
     // return React.createElement('div',{className: 'App'}, React.createElement('h1', null,'Hi, I am a React App!!!' ))
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,19 @@
+/*
+
+Lecture Notes:
+
+1.  Recommend use of arrow function syntax to get advantages of using "this" keyword.
+2.  Make sure to import React from 'react'
+3.  Make sure to export component using the export default syntax
+
+
+*/
+
+
+import React from 'react';
+
+const person = () => {
+    return <p>I'm a person</p>
+}
+
+export default person;
