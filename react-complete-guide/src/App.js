3f54379e4c7a99adbaaab7a035e9422dce5fefc6
Eric Simmons <eric.simmons@aarons.com>
2018-07-13 22:23:43 -0400

    Section 3, Lecture 39

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,39 @@
+
+/*
+
+Special property called 'state' for components that extend "Component"
+
+'state' is not available in function components
+
+using 'state' needs to be done with care.
+
+*/
+
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+
+  }
+
   render() {
     return (
       <div className="App">
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="35"/>
+        <h1>Hi, I am a React App</h1>
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={ this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={ this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={ this.state.persons[2].age} />
       </div>
     );
 
