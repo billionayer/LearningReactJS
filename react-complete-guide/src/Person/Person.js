51ade722ca4922b6d15824c26eba7e83adcd6156
Eric Simmons <eric.simmons@aarons.com>
2018-07-13 23:31:03 -0400

    Section 3, Lecture 46

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -23,10 +23,11 @@ Lecture Notes:
 
 
 import React from 'react';
+import './Person.css';
 
 const person = ( props ) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am { props.age } years old</p> {/*wire onClick to property*/}
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
